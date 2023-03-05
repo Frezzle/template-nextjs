9a6a891186a0305528acc40227915555285688c0
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-03-05 21:27:06 +0000

    add vercel Analytics, specifically Audiences (beta)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import styles from '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
