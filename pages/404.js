7ced03843bd87b78565008a6ccdbd2b6d314e86e
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-03-05 19:38:27 +0000

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <h2 className={utilStyles.headingLg}>I can't find it, you sure I borrowed it?</h2>
+      </section>
+    </Layout>
+  );
+}
