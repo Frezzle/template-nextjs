648da039b6bb22b96cd1be5f205651520e33697e
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-03-05 15:46:37 +0000

    assets, metadata, global css, scoped css modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,15 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>My First Post</title>
+      </Head>
       <Link href="/">Go back home</Link>
       <h1>First Post</h1>
-    </>
+    </Layout>
   );
 }
