cf60ec6a5da046dc20092f3d9cedea5e31bed4c2
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-03-05 21:51:50 +0000

    change welcome message, test vercel preview (#1)

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Fred, doing a Next.js tutorial.</p>
+        <p>Fred here, Next.js'ing 🤙</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website built using
+          {' '}
+          <a href="https://nextjs.org/learn">the Next.js tutorial</a>)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
