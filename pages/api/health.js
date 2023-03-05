a86e418db5f2731eac800742f038cac4f935098a
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-03-05 21:08:15 +0000

    static api routes, dynamic api routes

---
+++ pages/api/health.js
@@ -0,0 +1,15 @@
+import api from '../../lib/api';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: "Only GET plz thx." });
+  }
+
+  const isHealthy = await api.getHealth();
+  if (!isHealthy) {
+    // realistically this wouldn't execute but just example
+    res.status(500).json({ text: "OKAY NOW YOU CAN PANIC" });
+  }
+
+  res.status(200).json({ text: "All good in the hood." });
+}
