157813720b8bc226b8a423ca3284b7611cb66279
Frederico Mazzone <5685776+Frezzle@users.noreply.github.com>
2023-04-17 22:09:26 +0100

    fix header style lint

--- components/header.tsx
+++ components/header.tsx
@@ -14,20 +14,20 @@ export default function Header() {
       <noscript>
         <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
       </noscript>
-      <div className={styles.signedInStatus}>
+      <div className={styles['signedInStatus']}>
         <p
           className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
+            !session && loading ? styles['loading'] : styles['loaded']
           }`}
         >
           {!session && (
             <>
-              <span className={styles.notSignedInText}>
+              <span className={styles['notSignedInText']}>
                 You are not signed in
               </span>
               <a
                 href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
+                className={styles['buttonPrimary']}
                 onClick={(e) => {
                   e.preventDefault();
                   signIn();
@@ -42,10 +42,10 @@ export default function Header() {
               {session.user.image && (
                 <span
                   style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
+                  className={styles['avatar']}
                 />
               )}
-              <span className={styles.signedInText}>
+              <span className={styles['signedInText']}>
                 <small>Signed in as</small>
                 <br />
                 {session.user.email && (
@@ -55,7 +55,7 @@ export default function Header() {
               </span>
               <a
                 href={`/api/auth/signout`}
-                className={styles.button}
+                className={styles['button']}
                 onClick={(e) => {
                   e.preventDefault();
                   signOut();
@@ -68,26 +68,26 @@ export default function Header() {
         </p>
       </div>
       <nav>
-        <ul className={styles.navItems}>
-          <li className={styles.navItem}>
+        <ul className={styles['navItems']}>
+          <li className={styles['navItem']}>
             <Link href="/">Home</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/client">Client</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/server">Server</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/protected">Protected</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/api-example">API</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/admin">Admin</Link>
           </li>
-          <li className={styles.navItem}>
+          <li className={styles['navItem']}>
             <Link href="/me">Me</Link>
           </li>
         </ul>
